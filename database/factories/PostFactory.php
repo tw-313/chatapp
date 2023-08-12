3436763c6f2e91da010cb6315415df3333e387dd
estack-wakayama-takuya <110365589+estack-wakayama-takuya@users.noreply.github.com>
2023-08-12 21:06:29 +0900

    Postactoryのファイル設定

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -10,6 +11,12 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class PostFactory extends Factory
 {
     /**
+     * ポストモデルクラス
+     */
+    protected $post = Post::class;
+
+
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
@@ -17,7 +24,15 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'created_at' => $this->faker->date('Y-m-d H:i:s', 'now'),
+            'updated_at' => $this->faker->date('Y-m-d H:i:s', 'now'),
+            'subject' => $this->faker->realText(16),
+            // 16文字のテキスト
+            'message' => $this->faker->realText(200),
+            // 200文字のテキスト
+            'name' => $this->faker->name,
+            // 氏名
+            'category_id' => $this->faker->numberBetween(1, 5),
         ];
     }
 }
